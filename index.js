ade249e78a990a69496ea38b9d2933ad2a0cdbac
pooyadhgh <pooya.dhgh@gmail.com>
2021-11-14 17:27:11 +0330

    Add graphql query

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
 
 const postsRoutes = require('./routes/postsRoutes');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolvers = require('./graphql/resolvers');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+// REST API Middleware
 app.use('/posts', postsRoutes);
+
+// GraphQL Middlewqre
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
 
 app.listen(5000, console.log('Connected'));
